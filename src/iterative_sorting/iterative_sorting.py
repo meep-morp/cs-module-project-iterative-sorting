31b24670c9428ca21d131cd5dd22020f2ef04421
meep-morp <gracieaerogirl@gmail.com>
2020-08-17 12:28:29 -0500

    Finished Searching MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -45,6 +45,6 @@ What is the time and space complexity of the counting sort algorithm?
 
 
 def counting_sort(arr, maximum=None):
-    # Your code here
+    count = []
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,34 @@
-def linear_search(arr, target):
-    # Your code here
+from iterative_sorting import selection_sort
 
+
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
+# Compare the item in the middle of the data set to the item we are searching for.
+    # If it is the same, stop. We found it and are done!
+    # Else, if the item we are searching for is LESS than the item in the middle:
+    # Eliminate the RHS of list. Repeat step 1 with only the LHS of list.
+    # Else, the item we are searching for is GREATER than the item in the middle:
+    # Eliminate the LHS of list. Repeat step 1 with only the RHS of the list.
+
+
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
-    return -1  # not found
+    first = 0
+    last = len(arr)-1
+    found = -1
+    while(first <= last and found == -1):
+        mid = (first + last)//2
+        if arr[mid] == target:
+            found = mid
+        else:
+            if target < arr[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return found
